c6ffaa2c60f033b32dbeb0532a8f8702a73692fe
Rami <Dunadan2007@yandex.ru>
2022-12-17 22:32:57 +0300

    ZenJect installed

--- GbStrategy/Assets/Root/Modules/UserControlSystem/Commands/Model/PatrolCommand.cs
+++
@@ -1,9 +0,0 @@
-using Abstractions.Assets.Root.Modules.Abstractions;
-
-namespace Assets.Root.Modules.UserControlSystem.Commands
-{
-    public class PatrolCommand : IPatrolCommand
-    {
-
-    }
-}
