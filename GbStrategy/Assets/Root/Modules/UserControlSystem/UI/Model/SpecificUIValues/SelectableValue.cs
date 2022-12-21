6cf6fd53042fa4a2397afcdc6de6fa1674da3d97
Rami <Dunadan2007@yandex.ru>
2022-12-21 23:03:30 +0300

    HW3 part 3 ready

---
+++ GbStrategy/Assets/Root/Modules/UserControlSystem/UI/Model/SpecificUIValues/SelectableValue.cs
@@ -0,0 +1,9 @@
+using Abstractions.Assets.Root.Modules.Abstractions;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(SelectableValue), menuName = "Strategy Game/" + nameof(SelectableValue), order = 0)]
+
+public class SelectableValue : UIValue<ISelectable>
+{
+
+}
---
+++ GbStrategy/Assets/Root/Modules/UserControlSystem/UI/Model/SpecificUIValues/VictimValue.cs
@@ -0,0 +1,12 @@
+using Assets.Root.Modules.Abstractions;
+using UnityEngine;
+
+namespace Assets.Root.Modules.UserControlSystem.UI.Model
+{
+    [CreateAssetMenu(fileName = nameof(VictimValue), menuName = "Strategy Game/" + nameof(VictimValue), order = 0)]
+
+    public class VictimValue : UIValue<IVictim>
+    {
+
+    }
+}
