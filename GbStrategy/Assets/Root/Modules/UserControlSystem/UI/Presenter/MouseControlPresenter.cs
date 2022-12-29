51935aafc2a31fa971245aea4b9493f8b44376a1
Ramio7 <Dunadan2007@yandex.ru>
2022-12-29 13:56:36 +0300

    HW5 part 1 ready
    MouseControlPresenter edited

--- GbStrategy/Assets/Root/Modules/UserControlSystem/UI/Presenter/MouseControlPresenter.cs
+++ GbStrategy/Assets/Root/Modules/UserControlSystem/UI/Presenter/MouseControlPresenter.cs
@@ -2,6 +2,7 @@ using Abstractions.Assets.Root.Modules.Abstractions;
 using Assets.Root.Modules.Abstractions;
 using Assets.Root.Modules.UserControlSystem.UI.Model;
 using System.Linq;
+using UniRx;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Zenject;
@@ -13,24 +14,25 @@ namespace Assets.Root.Modules.UserControlSystem.UI.Presenter
         [SerializeField] private Camera _camera;
         [SerializeField] private SelectableValue _selectedObject;
         [SerializeField] private EventSystem _eventSystem;
+        [SerializeField] private Transform _groundTransform;
 
         [Inject] private Vector3Value _groundClicksRMB;
         [Inject] private VictimValue _victimRMB;
-        [SerializeField] private Transform _groundTransform;
 
         private Plane _groundPlane;
 
         private void Start()
         {
             _groundPlane = new Plane(_groundTransform.up, 0);
+            var leftClickStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonUp(0));
+            var rightClickStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonUp(1));
+
+            leftClickStream.Subscribe(_ => MouseSelect());
+            rightClickStream.Subscribe(_ => ExecuteCommand());
         }
 
-        private void Update()
+        private void MouseSelect()
         {
-            if (!Input.GetMouseButtonUp(0) && !Input.GetMouseButton(1))
-            {
-                return;
-            }
             if (_eventSystem.IsPointerOverGameObject())
             {
                 return;
@@ -38,25 +40,30 @@ namespace Assets.Root.Modules.UserControlSystem.UI.Presenter
             var ray = _camera.ScreenPointToRay(Input.mousePosition);
             var hits = Physics.RaycastAll(ray);
 
-            if (Input.GetMouseButtonUp(0))
+            if (RaycastTypeHandler<ISelectable>(hits, out var selectable))
             {
-                if (RaycastTypeHandler<ISelectable>(hits, out var selectable))
-                {
-                    _selectedObject.SetValue(selectable);
-                }
+                _selectedObject.SetValue(selectable);
             }
-            else
+        }
+
+        private void ExecuteCommand()
+        {
+            if (_eventSystem.IsPointerOverGameObject())
             {
-                if (RaycastTypeHandler<IVictim>(hits, out var victim))
-                {
-                    _victimRMB.SetValue(victim);
-                }
-                else if (_groundPlane.Raycast(ray, out var enter))
-                {
-                    _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
-                }
+                return;
             }
 
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            var hits = Physics.RaycastAll(ray);
+
+            if (RaycastTypeHandler<IVictim>(hits, out var victim))
+            {
+                _victimRMB.SetValue(victim);
+            }
+            else if (_groundPlane.Raycast(ray, out var enter))
+            {
+                _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
+            }
         }
 
         private bool RaycastTypeHandler<T>(RaycastHit[] hits, out T result) where T : class
