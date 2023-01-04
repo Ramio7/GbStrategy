a6d069adeae08b00874209327c0d2898719c4fe5
Rami <Dunadan2007@yandex.ru>
2023-01-04 13:34:19 +0300

    HW7 part 1 ready

--- GbStrategy/Assets/Root/Modules/Core/CommandExecutors/PatrolCommandExecutor.cs
+++ GbStrategy/Assets/Root/Modules/Core/CommandExecutors/PatrolCommandExecutor.cs
@@ -1,6 +1,9 @@
 using Abstractions.Assets.Root.Modules.Abstractions;
+using Assets.Root.Modules.Utils;
+using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Root.Modules.Core.CommandExecutors
 {
@@ -8,10 +11,34 @@ namespace Assets.Root.Modules.Core.CommandExecutors
     {
         [SerializeField] private UnitMovementStop _stop;
         [SerializeField] private Animator _animator;
+        [SerializeField] private StopCommandExecutor _stopCommandExecutor;
 
         public override async Task ExecuteSpecificCommand(IPatrolCommand command)
         {
-            Debug.Log($"{name} patrols from {command.Start} to {command.Target}");
+            var point1 = command.Start;
+            var point2 = command.Target;
+            while (true)
+            {
+                GetComponent<NavMeshAgent>().destination = point2;
+                _animator.SetTrigger("Walk");
+                _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+                try
+                {
+                    await _stop.WithCancellation(_stopCommandExecutor.CancellationTokenSource.Token);
+                }
+                catch
+                {
+                    GetComponent<NavMeshAgent>().isStopped = true;
+                    GetComponent<NavMeshAgent>().ResetPath();
+                    break;
+                }
+                var temp = point1;
+                point1 = point2;
+                point2 = temp;
+            }
+            _stopCommandExecutor.CancellationTokenSource = null;
+            _animator.SetTrigger("Idle");
         }
     }
+
 }
